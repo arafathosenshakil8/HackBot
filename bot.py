e5c9d61d58b1e08cd2aac5912cf31a88feb8e634
arafathosenshakil8 <148902346+arafathosenshakil8@users.noreply.github.com>
2026-07-29 19:01:26 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = "
-8581805593:AAFhjjFKzq0Lah5nYx4tO3Ud8LOn5GdD3Ww"
+TOKEN =
+"8581805593:AAFhjjFKzq0Lah5nYx4tO3Ud8LOn5GdD3Ww"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello! HackBot is running.")
